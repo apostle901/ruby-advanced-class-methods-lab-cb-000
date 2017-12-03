3b8ee5d58f54ae6d1696353b821f3585e8ac1d09
Jawann <jawann@gmail.com>
2017-12-03 21:06:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,26 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    song = Song.all.detect{|artist| artist.name == name}
+    song
+  end
+
 end
